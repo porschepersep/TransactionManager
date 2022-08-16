93d1c967b9e7a98ec91ec484e0467c4da67c3738
porschepersep <porsche@persep.co>
2022-08-16 15:24:01 -0500

    adding TransactionManager

--- main.py
+++ main.py
@@ -235,5 +235,5 @@ def transaction_program():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     transaction_program()
-
+ 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
